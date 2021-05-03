73959a571f0acaaee69aa0968e276dd862f9754d
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-05-02 21:10:45 -0700

    broken but not done yet

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ ctx.canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
-
+const defenders = [];
+let numberOfResources = 300;
 //mouse
 const mouse = {
     x: 10,
@@ -37,7 +38,7 @@ class Cell {
         this.height = cellSize;
     }
     draw(){
-        if(collision(this, mouse)){
+        if(mouse.x && mouse.y && collision(this, mouse)){
             ctx.strokeStyle = 'black';
             ctx.strokeRect(this.x, this.y, this.width, this.height);
         }
@@ -59,18 +60,56 @@ function handleGameGrid(){
 }
 //projectiles
 //defenders
+class Defender {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;
+        this.health = 100;
+        this.projectiles = [];
+        this.time = 0;
+    }
+    draw(){
+        ctx.fillStyle ='blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '20px Arial';
+        ctx.fillText(Math.floor(this.health), this.x, this.y);
+    }
+}
+canvas.addEventListener('click', function(){
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if(gridPositionY < cellSize) return;
+    let defenderCost = 100;
+    if(numberOfResources > defenderCost){
+        defenders.push(new Defender(gridPositionX, gridPositionY));
+        numberOfResources -= defenderCost;
+    }
+
+})
+function handleDefenders(){
+    for(let i = 0; i < defenders.length; i++){
+        defenders[i].draw;
+    }
+}
+
 //enemies
 //resources
 //utilities
 function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue'
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
     handleGameGrid();
+    handleDefenders();
     requestAnimationFrame(animate);
 }
 animate();
 
-
+console.log(defenders)
 
 function collision(first, second){
     if(   !(first.x > second.x + second.width ||
