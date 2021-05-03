2293fad79cb0ccb3d4150a9496b839bd3f05034f
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-05-02 18:34:04 -0700

    working collision detection

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 
+//mouse
+const mouse = {
+    x: 10,
+    y: 10,
+    width: 0.1,
+    height: 0.1,
+}
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(e){
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function(){
+    mouse.x = undefined;
+    mouse.y = undefined;
+})
 //game board
 const controlsBar = {
     width: canvas.width,
@@ -21,8 +37,11 @@ class Cell {
         this.height = cellSize;
     }
     draw(){
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        if(collision(this, mouse)){
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+        
     }
 }
 function createGrid(){
@@ -50,3 +69,15 @@ function animate(){
     requestAnimationFrame(animate);
 }
 animate();
+
+
+
+function collision(first, second){
+    if(   !(first.x > second.x + second.width ||
+            first.x + first.width < second.x ||
+            first.y > second.y + second.height ||
+            first.y + first.height < second.y)
+    ) {
+        return true;
+    };
+};
